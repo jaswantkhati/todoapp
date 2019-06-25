9f21ba9a91fe6c3f867c27436a33a998a97c0700
jaswantkhati <jaswant@excellencetechnologies.info>
2019-06-25 17:16:30 +0530

    add list part completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { ListTodoComponent } from './components/list-todo/list-todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "show-list",
+    pathMatch: "full"
+  },
+  {
+    path: "show-list",
+    component: ListTodoComponent
+  },
+  {
+    path: 'add-todo',
+    component: AddTodoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
